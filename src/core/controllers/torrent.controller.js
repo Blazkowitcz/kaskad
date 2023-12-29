1506349b5a783cfbe3c597100845864f30a66c5a
Blazkowitcz <bonnet.nicolas66@gmail.com>
2023-12-29 23:20:27 +0100

    Add torrent service and create/download torrent

--- src/core/controllers/torrent.controller.js
+++ src/core/controllers/torrent.controller.js
@@ -1,3 +1,7 @@
+const parse_torrent = require('parse-torrent');
+const crypto = require("crypto");
+const slug = require('slug');
+const fs = require('fs');
 const TorrentService = require('../services/torrent.service');
 
 /**
@@ -18,4 +22,42 @@ exports.getLastTorrents = async (req, res) => {
 exports.getBestTorrents = async (req, res) => {
     const results = await TorrentService.getBestTorrents();
     res.status(200).json(results);
+}
+
+/**
+ * Upload a new Torrent
+ * @param {Request} req 
+ * @param {Response} res 
+ */
+exports.upload = async (req, res) => {
+    const body = req.body;
+    const data = parse_torrent(req.files.torrent.data);
+    let torrent = await TorrentService.getTorrentByHash(data.infoHash);
+    if (torrent === null){
+        const file = req.files.torrent;
+        const filename = `${crypto.randomBytes(16).toString('hex')}.torrent`;
+        torrent = await TorrentService.createTorrent({
+            name: data.name,
+            slug: slug(data.name),
+            description: body.description,
+            filename: filename,
+            hash: data.infoHash
+        });
+        file.mv(`./public/torrents/${filename}`);
+    }
+    res.status(200).json({});
+}
+
+/**
+ * Download a Torrent
+ * @param {Request} req 
+ * @param {Response} res 
+ */
+exports.download = async (req, res) => {
+    const torrent = await TorrentService.getTorrent(req.params.id);
+    if(torrent === null){
+        return res.status(422).json({});
+    }
+    const data = parse_torrent(fs.readFileSync(`./public/torrents/${torrent.filename}`));
+    
 }
--- src/core/services/torrent.service.js
+++ src/core/services/torrent.service.js
@@ -15,3 +15,26 @@ exports.getLastTorrents = async () => {
 exports.getBestTorrents = async () => {
     return await TorrentModel.find().sort({'completed': +1}).select('-__v').limit(20);
 }
+
+/**
+ * Get Torrent by its id
+ * @param {String} id
+ * @returns {TorrentModel}
+ */
+exports.getTorrent = async (id) => {
+    return await TorrentModel.findOne({_id: id}).lean();
+}
+
+/**
+ * Get Torrent by its hash
+ * @param {String} hash 
+ * @returns {TorrentModel}
+ */
+exports.getTorrentByHash = async (hash) => {
+    return await TorrentModel.findOne({hash: hash}).lean();
+}
+
+exports.createTorrent = async (data) => {
+    const torrent = new TorrentModel(data);
+    return await torrent.save();
+}
