642dec51c25077b8afcba41fda61b5b8ebd72683
Blazkowitcz <bonnet.nicolas66@gmail.com>
2024-03-23 12:17:17 +0100

    Add authentification middleware

---
+++ src/core/middlewares/auth.middleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const config = require('../../config.json');
+
+/**
+ * Check token sent by user for authorization
+ * @param {Request} req 
+ * @param {Result} res 
+ * @param {Function} next 
+ * @returns 
+ */
+ exports.checkToken = function (req, res, next) {
+    let token = req.header('token');
+    if(!token) { return res.status(401).json({message: 'Auth Error'});}
+    try{
+        let decoded = jwt.verify(token, config.salt);
+        req.user = decoded.user;
+        next();
+    } catch (e) {
+        res.status(401).send({message: 'Invalid Token'});
+    }
+}
